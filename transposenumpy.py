ae59541b0ab76e30d446f7a604e22d01659c4e5c
Bhowmickk <bhowmicknarayanan4@gmail.com>
2025-06-18 21:20:06 +0530

    Add files via upload

---
+++ transposenumpy.py
@@ -0,0 +1,12 @@
+import numpy as np
+order=input("Enter the order of the matrix.\n")
+r,c=map(int,order.lower().split('x'))
+a=[]
+for i in range(r):
+    row=[]
+    for j in range(c):
+        temp=int(input("Enter the element.\n"))
+        row.append(temp)
+    a.append(row)
+a=np.array(a)    
+print(a.T)        
